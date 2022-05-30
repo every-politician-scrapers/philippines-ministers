78e39cfc3f3c85304c3d26df6d313cc9404d915e
Tony Bowden <tony@tmtm.com>
2022-05-30 11:14:18 +0300

    refresh data

--- sources/solicitor-general/scraper.rb
+++ sources/solicitor-general/scraper.rb
@@ -17,10 +17,6 @@ class OfficeholderList < OfficeholderListBase
     def columns
       %w[name _ dates].freeze
     end
-
-    def empty?
-      super || (startDate[0...4].to_i < 1998)
-    end
   end
 end
 
