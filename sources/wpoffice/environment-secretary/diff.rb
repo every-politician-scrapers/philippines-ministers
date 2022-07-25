ec47c20840996141f4d613ce5729870e12775990
Tony Bowden <tony@tmtm.com>
2022-07-25 11:48:58 +0300

    update to latest wpoffice

--- sources/wpoffice/environment-secretary/diff.rb
+++ sources/wpoffice/environment-secretary/diff.rb
@@ -3,5 +3,10 @@
 
 require 'every_politician_scraper/comparison'
 
-diff = EveryPoliticianScraper::DecoratedComparison.new('wikidata.csv', 'scraped.csv').diff
+# TODO: pass the list of decorators as an argument
+diff = EveryPoliticianScraper::Comparison.new('wikidata.csv', 'scraped.csv').diff
+diff = DaffDiff::Decorator.new(data: diff, cell_class: DaffDiff::Decorator::DatePrecision).decorated
+diff = DaffDiff::Decorator.new(data: diff, cell_class: DaffDiff::Decorator::Nullless).decorated
+diff = DaffDiff::Decorator.new(data: diff, cell_class: DaffDiff::Decorator::WithinWeek).decorated
+
 puts diff.sort_by { |r| [r.first, r[1].to_s] }.reverse.map(&:to_csv)
